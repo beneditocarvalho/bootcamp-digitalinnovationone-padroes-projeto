6eae64ee5d60e85660a9398b95e7a95dd51f5561
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-19 11:07:57 -0300

    Added Test for Design Pattern Strategy

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -3,11 +3,14 @@ package one.digitalinnovation.gof;
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
 
 public class Test {
 
 	public static void main(String[] args) {
-		
+
+		//Singleton
+
 		SingletonLazy lazy = SingletonLazy.getInstancia();
 		System.out.println(lazy);
 		lazy = SingletonLazy.getInstancia();
@@ -22,7 +25,24 @@ public class Test {
 		System.out.println(lazyHolder);
 		lazyHolder = SingletonLazyHolder.getInstancia();
 		System.out.println(lazyHolder);
-		
+
+		// Strategy
+
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(normal);
+
 	}
 
 }
